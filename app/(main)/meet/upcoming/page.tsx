3df49cc81664d9a83985bf0bfb7bd9ad4e1a620f
hammoh7 <soahammohaadkar@gmail.com>
2024-04-01 01:38:14 +0530

    Meet Components

---
+++ app/(main)/meet/upcoming/page.tsx
@@ -0,0 +1,11 @@
+const UpcomingPage = () => {
+    return ( 
+        <section className="flex size-full flex-col gap-10 text-white">
+            <h1 className="ttext-2xl font-bold">
+                Upcoming Meetings
+            </h1>
+        </section>
+     );
+}
+ 
+export default UpcomingPage;
