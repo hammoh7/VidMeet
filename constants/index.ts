51a1c41bdb1245eedb380d88a61417396f20b503
hammoh7 <soahammohaadkar@gmail.com>
2024-04-01 01:39:35 +0530

    Constants

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarConstants = [
+  {
+    label: "HOME",
+    route: "/meet",
+    imageUrl: "/images/Home.jpg",
+  },
+  {
+    label: "UPCOMING",
+    route: "/meet/upcoming",
+    imageUrl: "/images/Upcoming.jpg",
+  },
+  {
+    label: "PREVIOUS",
+    route: "/meet/previous",
+    imageUrl: "/images/Previous.jpg",
+  },
+  {
+    label: "RECORDINGS",
+    route: "/meet/recordings",
+    imageUrl: "/images/Recording.jpg",
+  },
+  {
+    label: "PERSONAL",
+    route: "/meet/personal",
+    imageUrl: "/images/Personal.jpg",
+  },
+];
