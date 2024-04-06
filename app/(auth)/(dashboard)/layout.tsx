599ea1af5be48aba6354c85467b5e8a910aba2b1
hammoh7 <soahammohaadkar@gmail.com>
2024-04-06 19:14:24 +0530

    Dashboard layout

---
+++ app/(auth)/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+const DashboardLayout = ({
+    children
+}: {children: React.ReactNode}) => {
+    return ( 
+        <div className="h-full">
+            {children}
+        </div>
+     );
+}
+ 
+export default DashboardLayout;
