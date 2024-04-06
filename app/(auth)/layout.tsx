32a1eb46170f2d02c7454dd510c67a8a08a14c7c
hammoh7 <soahammohaadkar@gmail.com>
2024-04-06 19:15:07 +0530

    Auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
+
+export default AuthLayout;
