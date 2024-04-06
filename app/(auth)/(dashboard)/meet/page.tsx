e0372d38fe9bf41f395a62b006047f75f635711e
hammoh7 <soahammohaadkar@gmail.com>
2024-04-06 19:12:43 +0530

    Meeting Dashboard

---
+++ app/(auth)/(dashboard)/meet/page.tsx
@@ -0,0 +1,11 @@
+const MeetPage = () => {
+    return ( 
+        <section className="flex size-full flex-col gap-10 text-white">
+            <h1 className="ttext-2xl font-bold">
+                Home
+            </h1>
+        </section>
+     );
+}
+ 
+export default MeetPage;
---
+++ app/(auth)/(dashboard)/meet/recordings/page.tsx
@@ -0,0 +1,11 @@
+const RecordingsPage = () => {
+    return ( 
+        <section className="flex size-full flex-col gap-10 text-white">
+            <h1 className="ttext-2xl font-bold">
+                Recording page
+            </h1>
+        </section>
+     );
+}
+ 
+export default RecordingsPage;
