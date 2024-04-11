256309b038b6e7ea6eb2a3ecb28a4b70de291eb9
hammoh7 <soahammohaadkar@gmail.com>
2024-04-11 16:45:52 +0530

    Middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
-  afterAuth(auth, req) {
-    if (auth.session) {
-      let path = "/meet";
-    }
-  }
 });
 
 export const config = {
