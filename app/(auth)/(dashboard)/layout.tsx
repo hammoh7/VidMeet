d5d13af271d3316947951e9fca59b2391321b910
hammoh7 <soahammohaadkar@gmail.com>
2024-04-15 20:51:31 +0530

    Dashboard

--- app/(auth)/(dashboard)/layout.tsx
+++ app/(auth)/(dashboard)/layout.tsx
@@ -1,11 +1,11 @@
-const DashboardLayout = ({
-    children
-}: {children: React.ReactNode}) => {
-    return ( 
-        <div className="h-full">
-            {children}
-        </div>
-     );
-}
- 
+import ClientProvider from "@/providers/ClientProvider";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <ClientProvider>{children}</ClientProvider>
+    </div>
+  );
+};
+
 export default DashboardLayout;
