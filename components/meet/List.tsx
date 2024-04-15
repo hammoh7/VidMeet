e905170549bfef80d4936980159dcb6684b8e8d0
hammoh7 <soahammohaadkar@gmail.com>
2024-04-15 20:57:17 +0530

    Components

--- components/meet/List.tsx
+++ components/meet/List.tsx
@@ -4,12 +4,58 @@ import { CalendarCheck2, LucideVideo, SaveIcon, UserPlus2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Meeting from "./Meeting";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "../ui/use-toast";
 
 const MeetList = () => {
   const router = useRouter();
-  const [meeting, setMeeting] = useState<'isNewMeeting' | 'isJoiningMeeting' | 'isScheduleMeeting' | undefined>()
-  const createMeeting = () => {
+  const [meeting, setMeeting] = useState<'isNewMeeting' | 'isJoiningMeeting' | 'isScheduleMeeting' | undefined>();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: '',
+  })
+  const [callDetails, setCallDetails] = useState<Call>()
+  const { toast } = useToast();
+  const createMeeting = async () => {
+    if (!client || !user) return;
 
+    try {
+      if (!values.dateTime) {
+        toast({ title: "Please select Date and Time" })
+        return;
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+      if (!call) throw new Error('Call missing!');
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'New Meeting';
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description
+          }
+        }
+      })
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+
+      toast({ title: "Meeting Created" })
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to Create Meeting",
+        description: "Please try again",
+      })
+    }
   }
   
   return (
--- components/meet/Navbar.tsx
+++ components/meet/Navbar.tsx
@@ -1,19 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
 import SideNavbarMobile from "./SideNavbar";
 import { UserButton } from "@clerk/nextjs";
+import { Video } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="flex flex-between fixed z-50 w-full bg-slate-700 px-5 py-5 lg:px-10">
       <Link href="/meet" className="flex items-center gap-1">
-        <Image
-          src="/images/logo.jpg"
-          alt="Logo"
-          className="max-sm:size-10"
-          width={25}
-          height={25}
-        />
+        <Video className="h-8 w-8 text-white mr-2" />
         <p className="text-[24px] font-bold text-white max-sm:hidden">
           VidMeet
         </p>
