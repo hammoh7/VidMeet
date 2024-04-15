4a5658522689de7a9331d2f9621ad16f532e557e
hammoh7 <soahammohaadkar@gmail.com>
2024-04-15 20:55:45 +0530

    Components

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+import { Loader2 } from "lucide-react"
+
+export const Loader = () => {
+    return (
+        <div className="flex-center h-screen w-full">
+            <Loader2 className="h-5 w-5" />
+        </div>
+    )
+}
