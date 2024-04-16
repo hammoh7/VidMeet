c91f1b2e0abfac089f06fdfb0fdb379b5a05e2b0
hammoh7 <soahammohaadkar@gmail.com>
2024-04-16 13:31:35 +0530

    Dashboard

--- app/(auth)/(dashboard)/meeting/[id]/page.tsx
+++ app/(auth)/(dashboard)/meeting/[id]/page.tsx
@@ -1,9 +1,33 @@
-const MeetingRoom = ({ params }: { params : { id: string } }) => {
+"use client";
+
+import { Loader } from "@/components/Loader";
+import MeetingRoom from "@/components/meet/MeetingRoom";
+import MeetingSetup from "@/components/meet/MeetingSetup";
+import { getCallById } from "@/hooks/getCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+
+const MeetingPage = ({ params: { id } }: { params : { id: string } }) => {
+    const { user, isLoaded } = useUser();
+    const [setupComplete, setSetupComplete] = useState(false);
+    const { call, callLoading } = getCallById(id);
+
+    if (!isLoaded || callLoading) return <Loader />;
+
     return ( 
-        <div>
-            Meeting Room: #{params.id}
+        <div className="h-screen w-full">
+            <StreamCall call={call}>
+                <StreamTheme>
+                    {!setupComplete ? (
+                        <MeetingSetup />
+                    ): (
+                        <MeetingRoom /> 
+                    )}
+                </StreamTheme>
+            </StreamCall>
         </div>
      );
 }
  
-export default MeetingRoom;
+export default MeetingPage;
