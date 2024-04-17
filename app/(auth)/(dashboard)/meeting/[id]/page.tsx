9b4bad485933250dfbf883195b3d41ae66df0a88
hammoh7 <soahammohaadkar@gmail.com>
2024-04-17 15:11:50 +0530

    Dashboard

--- app/(auth)/(dashboard)/meeting/[id]/page.tsx
+++ app/(auth)/(dashboard)/meeting/[id]/page.tsx
@@ -8,26 +8,26 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
-const MeetingPage = ({ params: { id } }: { params : { id: string } }) => {
-    const { user, isLoaded } = useUser();
-    const [setupComplete, setSetupComplete] = useState(false);
-    const { call, callLoading } = getCallById(id);
+const MeetingPage = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [setupComplete, setSetupComplete] = useState(false);
+  const { call, callLoading } = getCallById(id);
 
-    if (!isLoaded || callLoading) return <Loader />;
+  if (!isLoaded || callLoading) return <Loader />;
 
-    return ( 
-        <div className="h-screen w-full">
-            <StreamCall call={call}>
-                <StreamTheme>
-                    {!setupComplete ? (
-                        <MeetingSetup />
-                    ): (
-                        <MeetingRoom /> 
-                    )}
-                </StreamTheme>
-            </StreamCall>
-        </div>
-     );
-}
- 
+  return (
+    <div className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!setupComplete ? (
+            <MeetingSetup setSetupComplete={setSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </div>
+  );
+};
+
 export default MeetingPage;
