53262723a757135beca75c33a5533a675b35bf2e
hammoh7 <soahammohaadkar@gmail.com>
2024-05-04 23:02:45 +0530

    App

--- app/(auth)/(dashboard)/meet/upcoming/page.tsx
+++ app/(auth)/(dashboard)/meet/upcoming/page.tsx
@@ -1,9 +1,12 @@
+import MeetingList from "@/components/meet/MeetList";
+
 const UpcomingPage = () => {
     return ( 
         <section className="flex size-full flex-col gap-10 text-white">
             <h1 className="text-xl font-bold">
                 Upcoming Meetings
             </h1>
+            <MeetingList type="upcoming" />
         </section>
      );
 }
