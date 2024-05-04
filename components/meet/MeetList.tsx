6a5848d27bfdeb41a87fb6864b46fcc285e13ea7
hammoh7 <soahammohaadkar@gmail.com>
2024-05-04 23:02:23 +0530

    Meet

---
+++ components/meet/MeetList.tsx
@@ -0,0 +1,9 @@
+const MeetingList = ({ type }: { type: 'upcoming' | 'done' | 'recordings' }) => {
+    return ( 
+        <div>
+            Meeting List
+        </div>
+     );
+}
+ 
+export default MeetingList;
