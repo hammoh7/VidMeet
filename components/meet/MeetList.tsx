67dab376c01274806ae99958e82cbd28fe8e6367
hammoh7 <soahammohaadkar@gmail.com>
2024-05-06 23:20:28 +0530

    Components

--- components/meet/MeetList.tsx
+++ components/meet/MeetList.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useMeetCalls } from "@/hooks/getMeetCalls";
+import { useRouter } from "next/navigation";
+
 const MeetingList = ({ type }: { type: 'upcoming' | 'done' | 'recordings' }) => {
+    const { endedCalls, upcomingCalls, callRecordings, isLoading } = useMeetCalls();
+    const router = useRouter();
+
+    const getCalls = () => {
+
+    }
+
     return ( 
         <div>
             Meeting List
