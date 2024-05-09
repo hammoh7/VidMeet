ca60adaa132b1912c2aa2f7fa05e3c53760e8ef6
hammoh7 <soahammohaadkar@gmail.com>
2024-05-10 01:08:32 +0530

    Upcoming Meet

--- app/(auth)/(dashboard)/meet/upcoming/page.tsx
+++ app/(auth)/(dashboard)/meet/upcoming/page.tsx
@@ -1,14 +1,13 @@
-import MeetingList from "@/components/meet/MeetList";
+import MeetingList from "@/components/meet/MeetingList";
+
 
 const UpcomingPage = () => {
-    return ( 
-        <section className="flex size-full flex-col gap-10 text-white">
-            <h1 className="text-xl font-bold">
-                Upcoming Meetings
-            </h1>
-            <MeetingList type="upcoming" />
-        </section>
-     );
-}
- 
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-xl font-bold">Upcoming Meetings</h1>
+      <MeetingList type="upcoming" />
+    </section>
+  );
+};
+
 export default UpcomingPage;
