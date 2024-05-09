38c655d4266ca12e14e2d3cec2f729424dbf58ea
hammoh7 <soahammohaadkar@gmail.com>
2024-05-09 23:49:09 +0530

    Hooks

--- hooks/getMeetCalls.ts
+++ hooks/getMeetCalls.ts
@@ -2,7 +2,7 @@ import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useEffect, useState } from "react";
 
-export const useMeetCalls = () => {
+export const getMeetCalls = () => {
   const [calls, setCalls] = useState<Call[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const client = useStreamVideoClient();
@@ -15,7 +15,7 @@ export const useMeetCalls = () => {
 
       try {
         const { calls } = await client.queryCalls({
-          sort: [{ field: "startsAt", direction: -1 }],
+          sort: [{ field: "starts_at", direction: -1 }],
           filter_conditions: {
             startsAt: { $exists: true },
             $or: [
