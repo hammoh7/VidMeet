a92987829d25159f5eb7c7af0aa840752b1e06a7
hammoh7 <soahammohaadkar@gmail.com>
2024-05-10 01:08:14 +0530

    Meet History

--- app/(auth)/(dashboard)/meet/previous/page.tsx
+++ app/(auth)/(dashboard)/meet/previous/page.tsx
@@ -1,9 +1,12 @@
+import MeetingList from "@/components/meet/MeetingList";
+
 const PreviousPage = () => {
     return ( 
         <section className="flex size-full flex-col gap-10 text-white">
             <h1 className="text-xl font-bold">
                 Previous Meetings
             </h1>
+            <MeetingList type="done" />
         </section>
      );
 }
